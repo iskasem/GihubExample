338490071471b3ee1aa6dbf6289572afeba7c06f
Islam Kasem <is_kasem@yahoo.com>
2018-07-21 12:52:06 +0300

    Initial Commit

---
+++ GihubExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GihubExample
+//
+//  Created by Islam Kasem on 21/07/2018.
+//  Copyright © 2018 Islam Kasem. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
