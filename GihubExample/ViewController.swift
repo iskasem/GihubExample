6f1495939053322ce8ac799ea42eee7956f606ff
Islam Kasem <is_kasem@yahoo.com>
2018-07-21 13:36:48 +0300

    some changes done on Islam branch

--- GihubExample/ViewController.swift
+++ GihubExample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print ("some changes")
+        print ("some changes made in Islam branch")
     }
 
     override var representedObject: Any? {
